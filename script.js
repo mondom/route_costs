2ad783e62b4c54fb88116793658cd377ad7ec748
Monika <codingbymon@gmail.com>
2023-10-17 15:10:05 +0200

    Dodanie skryptów js, i stylowanie elementów after i before w CSS

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
+const priceInput = document.querySelector('#price')
+const combustionInput = document.querySelector('#combustion')
+const routeInput = document.querySelector('#route')
+const countBtn = document.querySelector('.count')
+const error = document.querySelector('.error')
+const costInfo = document.querySelector('.cost-info')
+const spanCost = document.querySelector('.cost')
 
 
 
+const dataCheck = () => {
+	if (priceInput.value === ''|| combustionInput.value === '' || routeInput.value === '') {
+		error.textContent = 'Uzupełnij wszystkie pola!'
+        costInfo.style.display = 'none'
+	} else {
+		costCalculation()
+		costInfo.style.display = 'flex'
+        error.textContent = ''
+	}
+}
+
+const costCalculation = () => {
+	const petrolPrice = priceInput.value
+	const averageCombustion = combustionInput.value
+	const routeLength = routeInput.value
+
+	const cost = (routeLength / (100 / averageCombustion)) * petrolPrice
+	spanCost.textContent = cost.toFixed(2)
+}
+
+
+
+countBtn.addEventListener('click', dataCheck)
